9e75f00e8173f1f2e6ed58a3e87821eece66d50d
Kunal Varma <Kunal Varma>
2017-01-08 21:44:22 +0530

    Style fixes

--- public/js/render.js
+++ public/js/render.js
@@ -1,3 +1,41 @@
 var render = function(data) {
+    var info = data.info;
+    var recommendations = data.recommendations;
 
+    var html = "<h4><a href='" + info.default.url + "'>" + info.title + "</a></h4>";
+    html += "<h5>" + info.default.price + "</h5>";
+    html += "<div class='prices'><h4 class='heading'>Prices</h4>";
+
+    info.allPrices.forEach(function(el) {
+        html += "<div class='price-item'>";
+        html += "<h5><a href='" + el.url + "'>" + el.vendor + "</a><b>" + el.price + "</b></h5>";
+        html += "</div>";
+    });
+
+    html += "</div>";
+
+    html += "<div class='recomms'>";
+    console.log(recommendations.length);
+    if (recommendations.length != undefined) {
+        recommendations.forEach(function (el) {
+            var html = '<div>';
+            html += "<h4><a href='" + el.default.url + "'>" + el.title + "</a></h4>";
+            html += "<h5>" + el.default.price + "</h5>";
+            html += "<div class='prices'><h4>Prices</h4>";
+
+            el.allPrices.forEach(function (elem) {
+                html += "<div class='price-item'>";
+                html += "<h4><a href='" + elem.url + "'>" + elem.vendor + "</a><b>" + elem.price + "</b></h4>";
+                html += "</div>";
+            });
+
+            html += "</div>";
+        });
+    }
+
+    console.log(html);
+    jQuery(".drawer").html(html);
+    jQuery(".drawer").removeClass("hide");
+    jQuery(".price-item:first-child").addClass("active");
+    console.log(data);
 };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,15 @@
             padding: 0;
         }
 
+        body {
+            font-family: "Roboto", sans-serif;
+            font-size: 16px;
+        }
+
+        .prod {
+            display: none;
+        }
+
         .select {
             position: fixed;
             z-index: 500;
@@ -22,7 +31,7 @@
             left: 0;
             right: 0;
             text-align: center;
-            background: #333;
+            background: #43339b;
             color: #fff;
             padding: 40px;
         }
@@ -90,6 +99,72 @@
         #output {
             display: none;
         }
+
+        video {
+            display: block;
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .drawer {
+            position: fixed;
+            bottom: 0;
+            background: rgba(255, 255, 255, 0.6);
+            left: 0;
+            right: 0;
+            padding: 20px;
+        }
+
+        .drawer a {
+            text-decoration: none;
+        }
+
+        .drawer .heading {
+            color: #38373c;
+            padding: 10px 0;
+        }
+
+        .prices h4 {
+            color: rgba(255, 255, 255, 0.55);
+            margin-bottom: 10px;
+        }
+
+        .price-item.active {
+            background: #dd378a;
+        }
+
+        .price-item h5 b {
+            color: rgba(255, 255, 255, 0.6);
+            float: right;
+        }
+
+        .price-item {
+            background: rgba(0, 0, 0, 0.3);
+            padding: 10px;
+            margin: 10px 0;
+        }
+
+        .price-item h5 a {
+            color: #fff;
+        }
+
+        .prices {
+            background: #4540a2;
+            padding: 20px;
+            color: #fff;
+        }
+
+        .prices h4 {
+            color: rgba(255, 255, 255, 0.55);
+            margin-bottom: 10px;
+        }
+
     </style>
 
 </head>
@@ -102,9 +177,15 @@
         <img src="./logo.png" class="logo">
         <img src="./loader.svg" class="loader">
     </div>
+    <div class="prod" id="p-title"></div>
+
     <video id="video" muted autoplay></video>
     <canvas id="canvas"></canvas>
     <ul id="output"></ul>
+
+    <div class="drawer hide">
+
+    </div>
 
     <div class="select" id="select">
         <select id="videoSource"></select>
@@ -112,6 +193,7 @@
         <select class="hide" id="audioSource"></select>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/render.js"></script>
     <script src="./js/popcorn.js"></script>
     <script src="./js/app.js"></script>
 </div>
